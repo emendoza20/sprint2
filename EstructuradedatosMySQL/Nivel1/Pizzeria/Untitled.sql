61fd97ff7581f1044c0485e78e161dd75a2f48f8
elizabeth <emendoza867@gmail.com>
2023-04-18 13:15:21 +0200

    changes

---
+++ EstructuradedatosMySQL/Nivel1/Pizzeria/Untitled.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `Cliente` (
+  `id` integer PRIMARY KEY,
+  `username` varchar(255),
+  `role` varchar(255),
+  `created_at` timestamp
+);
+
+CREATE TABLE `Pedidos` (
+  `numero_pedidos` integer PRIMARY KEY,
+  `fecha_hora` date,
+  `tipo_reparto` enum,
+  `precio_total` float,
+  `cant_productos` integer
+);
+
+CREATE TABLE `Productos` (
+  `id_producto` integer PRIMARY KEY,
+  `tipo_productos` enum,
+  `nombre` varchar(10),
+  `descripcion` varchar(20),
+  `precio` float,
+  `imagen` varbinary(max)
+);
+
+ALTER TABLE `Pedidos` ADD FOREIGN KEY (`numero_pedidos`) REFERENCES `Cliente` (`id`);
